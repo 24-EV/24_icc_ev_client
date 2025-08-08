b8ddf3cce36dcfb599eaf0504e9a4e9b5465021c
nitepp04 <sondh1497@gmail.com>
2025-08-09 04:18:54 +0900

    chore : 자잘한 코드 개선

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,9 +1,11 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:2004';
 export const KAKAO_MAP_API_KEY = import.meta.env.VITE_KAKAO_MAP_API_KEY;
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:2004';
 export const CONTROLLER_VERSION = import.meta.env.VITE_CONTROLLER_VERSION || 24;
+export const PORT = import.meta.env.VITE_PORT || 1101;
 
 export default {
-  SERVER_URL,
   KAKAO_MAP_API_KEY,
-  CONTROLLER_VERSION
+  SERVER_URL,
+  CONTROLLER_VERSION,
+  PORT
 };
